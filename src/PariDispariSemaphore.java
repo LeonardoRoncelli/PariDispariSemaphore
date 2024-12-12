bda09dcd81e1ec272f98792ab2a89a987af97a2b
Roncelli <leonardoroncelli@gmail.com>
2024-12-12 19:04:33 +0100

    Attributi, costruttore della classe e if che assegna in base al nome il valore iniziale della variabile conta.

--- src/PariDispariSemaphore.java
+++ src/PariDispariSemaphore.java
@@ -1,2 +1,16 @@
+import java.util.concurrent.Semaphore;
+
 public class PariDispariSemaphore {
+    int conta;
+    static Semaphore semaforoPari=new Semaphore(1);
+    static Semaphore semaforoDispari=new Semaphore(0);
+    String nome;
+    public PariDispariSemaphore(String nome){
+        this.nome=nome;
+        if(nome.equalsIgnoreCase("dispari")){
+            conta=1;
+        }else{
+            conta=0;
+        }
+    }
 }
