7f5666cb8db31d63117cbcd403fab109467e73d2
Roncelli <leonardoroncelli@gmail.com>
2024-12-12 19:08:07 +0100

    Aggiunta dei commenti agli attributi, ai semafori e al costruttore della classe, in particolare all'if.

--- src/PariDispariSemaphore.java
+++ src/PariDispariSemaphore.java
@@ -1,13 +1,14 @@
 import java.util.concurrent.Semaphore;
-
 public class PariDispariSemaphore {
+    //dichiarazione degli attributi della classe
     int conta;
+    String nome;
+    //creazione dei semafori
     static Semaphore semaforoPari=new Semaphore(1);
     static Semaphore semaforoDispari=new Semaphore(0);
-    String nome;
-    public PariDispariSemaphore(String nome){
+    public PariDispariSemaphore(String nome){ //costruttore della classe
         this.nome=nome;
-        if(nome.equalsIgnoreCase("dispari")){
+        if(nome.equalsIgnoreCase("dispari")){ //in questo if viene assegnato il primo valore alla variabile conta, tutto in base al contenuto della variabile nome
             conta=1;
         }else{
             conta=0;
