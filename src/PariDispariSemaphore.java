036a62df993b6c3590169d168f248596b59c5114
Roncelli <leonardoroncelli@gmail.com>
2024-12-12 19:17:34 +0100

    Metodo stampaNumdisPari con relativi commenti alle istruzioni.

--- src/PariDispariSemaphore.java
+++ src/PariDispariSemaphore.java
@@ -20,8 +20,19 @@ public class PariDispariSemaphore {
         try{
             semaforoPari.acquire(); //viene diminuito il numero di permessi di semaforoPari
             System.out.println(nome+": "+conta); //stampa del numero, ovvero la variabile conta
-            conta=conta+2; //viene aggiornata la variabile conta, in questo caso viene sommato 2 dato che siamo nella stampa relativa ai numeri pari
+            conta=conta+2; //viene aggiornata la variabile conta, viene sommato 2 dato che siamo nella stampa relativa ai numeri pari e la variabile conta per i numeri pari è inizializzata a 0, grazie all'if nel costruttore
             semaforoDispari.release(); //viene aumentato il numero dei permessi di semaforoDispari dato che è terminata la stampa del numero pari
+            TimeUnit.SECONDS.sleep(1); //attesa di un secondo per rendere l'output più leggibile
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public void stampaNumDispari(){
+        try{
+            semaforoDispari.acquire(); //viene diminuito il numero di permessi di semaforoDispari
+            System.out.println(nome+": "+conta); //stampa del numero, ovvero la variabile conta
+            conta=conta+2; //viene aggiornata la variabile conta, viene sommato 2 dato che siamo nella stampa relativa ai numeri pari e la variabile conta per i numeri pari è inizializzata a 0, grazie all'if nel costruttore
+            semaforoPari.release(); //viene aumentato il numero dei permessi di semaforoPari dato che è terminata la stampa del numero dispari
             TimeUnit.SECONDS.sleep(1); //attesa di un secondo per rendere l'output più leggibile
         }catch (Exception e){
             System.out.println(e);
